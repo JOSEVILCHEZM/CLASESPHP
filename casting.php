a66ef36077715fdde4b76ef346d341525270aacc
Jose Vilchez <jhtgus971454@gmail.com>
2022-05-29 18:44:45 -0500

    COMMIT NUEVOS DOS ARCHIVOS

---
+++ casting.php
@@ -0,0 +1,25 @@
+<?php
+
+/* $numero ="5";
+var_dump( $numero ); // dice que es un string
+$numero =(int) $numero; //estoy forzando una accion 
+var_dump( $numero); //ahora es un integer 
+echo"\n"; */
+
+/*$dias = 5.89; 
+$dias = (int) $dias;
+
+var_dump( $dias ); 
+
+echo "\n";*/
+
+/* $bandera = 0; 
+$bandera = (bool) $bandera;
+var_dump( $bandera); 
+echo "\n"; */
+
+$numero = 5 + "5"; 
+var_dump( $numero ); 
+echo "\n"; 
+
+
