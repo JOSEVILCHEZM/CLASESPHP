c7cfe0b49af2ef856d04c4e0e2fbf33ab77a30f5
Jose Vilchez <jhtgus971454@gmail.com>
2022-05-28 22:31:36 -0500

    TERCER COMMIT

---
+++ Tipodedato.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+/*
+
+TIPO DE DATO Integer: NUMERO QUE NUNCA TIENE DECIMALES
+Float: ES UN NUMERO DECIMAl o PUNTO FLOTANTE 
+Double: Decimales con valores más precisos, con más decimales que float.
+Cadenas ded caracteres:
+Chart: Un solo una letra o un simbolo.
+String: Una cadena de caracteres. CADENA DE LETRAS
+Booleanos:
+Bool: PUEDE SER Verdadero o falso.
+Sin valor:
+Null: No hay valor.
+Undefined: Hay una variable pero no tiene ningun valor
+
+*/
+
+
+/* $numero = "23" ; 
+$numero_numero =$numero + 2; 
+
+var_dump($nuevo_numero);
+var_dump($numero);
+echo "\n";
+*/
+
+$numero = 10; 
+$numero = $numero + 0.5; 
+
+var_dump($numero);
+
