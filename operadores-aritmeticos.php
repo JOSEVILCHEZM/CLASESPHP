9fc55b43a0311d15b069505001e8c3dc35f2252b
Jose Vilchez <jhtgus971454@gmail.com>
2022-05-30 00:12:51 -0500

    Operadores aritmeticos y relacionales

---
+++ operadores-aritmeticos.php
@@ -0,0 +1,23 @@
+<?php
+
+/* echo 5 + 5; 
+
+echo "\n"; */
+
+/* $resultado = 5 ** 3; 
+
+echo $resultado; 
+
+echo "\n"; */
+
+// echo (5 * 8). " " . (80 / 4); 
+
+/* $resultado = (5 * 8). " " . (80 / 4); 
+echo $resultado; 
+echo "\n";  */
+
+$horas = 7201; 
+echo "$horas segundos convertidos a horas son " . (7200 / 3600) . "\n";
+echo "Sobran " . ($horas % 3600) . "segundos.";
+echo "\n"; 
+
