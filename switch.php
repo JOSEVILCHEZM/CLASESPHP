77f21c5606976ea7044d3d8b4733bee3ca4b6583
Jose Vilchez <jhtgus971454@gmail.com>
2022-05-31 18:36:21 -0500

    switch

---
+++ switch.php
@@ -0,0 +1,38 @@
+<?php 
+
+// OBTENER NUMEROS FAVORITOS
+// Jose1 :9
+// Jose2 :4
+// Jose3 :3 
+// Jose4 :9
+// Jose5 :1
+$jose = 3; 
+
+switch($jose){
+
+    case 1: 
+        echo "Su numero favorito es el 9"; 
+        break; 
+        
+    case 2: 
+        echo "Su numero favorito es el 4"; 
+        break;
+
+    case 3: 
+        echo "Su numero favorito es el 3"; 
+        break; 
+            
+    case 4: 
+        echo "Su numero favorito es el 9"; 
+        break;
+    
+    case 5: 
+        echo "Su numero favorito es el 1"; 
+        break;
+
+    default: 
+        echo "No existe"; 
+
+}
+
+echo "\n";
