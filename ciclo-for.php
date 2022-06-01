e5e1d83444dcee593b051c9296665d27eaaa8481
Jose Vilchez <jhtgus971454@gmail.com>
2022-05-31 21:46:37 -0500

    ciclo for, foreach

---
+++ ciclo-for.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*for($contador = 0; $contador <10; $contador++){
+    //Escribir todo nuestro código
+    echo $contador . "\n";
+
+}
+
+echo"\n";*/
+
+for ($i = 0, $j= 0; $i <10; $i++ , $j +=2){
+    //Escribir todo nuestro código
+    echo "i= $i j =$j" . "\n";
+
+}
+
+echo"\n";
+
